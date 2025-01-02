619a15130971daec030b5e3d951046b6eba52b35
eminarikan <muhammeteminarikan@gmail.com>
2025-01-02 16:34:36 +0300

    changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -38,6 +38,8 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+vim.keymap.set('i', '<C-s>', '<cmd>:w<CR>')
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
