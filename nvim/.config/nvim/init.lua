43573af360b5d558de41ca5989077fc3566cd0c7
eminarikan <muhammeteminarikan@gmail.com>
2025-05-11 22:48:51 +0300

    changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -262,6 +262,7 @@ require('lazy').setup({
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
       'nvim-java/nvim-java',
+      'clangd/clangd',
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
